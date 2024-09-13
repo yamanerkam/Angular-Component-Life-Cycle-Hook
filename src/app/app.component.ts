4d00576bf112c41acfeec90393ffa0bcce18099a
yamanerkam <erkamyaman35@gmail.com>
2024-09-13 21:15:44 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'component-life-cycle-hook';
+
+
+
+
+
+  /*keyPressFunc($event: any) {
+    console.log('keyPressFunc');
+    console.log(event);
+  }
+  keyUpFunc($event: any) {
+    console.log(event);
+    console.log('keyUpFunc');
+  }
+  keyDownFunc($event: any) {
+    console.log(event);
+    console.log('keyDownFunc');
+  }*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageAComponent } from './components/page-a/page-a.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAComponent
   ],
   imports: [
     BrowserModule,
